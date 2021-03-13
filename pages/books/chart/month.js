6f47e778f1cfd4ab4d00d2fcfb1e37b3e42459c8
Simon Ng <mercury_sn@hotmail.com>
2021-03-13 23:02:55 +1100

    Adding in new page for charts

---
+++ pages/books/chart/month.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MonthChart from "../../../component/MonthChart";
+
+class Month extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Testing</h1>
+        <MonthChart></MonthChart>
+      </div>
+    );
+  };
+}
+
+export default Month;
